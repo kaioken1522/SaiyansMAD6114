28afe365ead3a401e69e1a1d6e7a384502e68087
Kaushil <c0826985@mylambton.ca>
2022-11-06 18:46:05 -0500

    login&reg done with whole backend

--- models/category.js
+++ models/category.js
@@ -1,7 +1,46 @@
+// const mongoose = require('mongoose');
+
+// const categorySchema = mongoose.Schema({
+//  name: {
+//     type: String,
+//     required: true,
+//   },
+//   color: {
+//       type: String,
+//       required: true,
+//   },
+//   icon : {
+//       type: String,
+//   },
+// //   image: {
+// //       type: String,
+// //   }
+// })
+
+// exports.Category = mongoose.model('Category', categorySchema);
+
 const mongoose = require('mongoose');
 
 const categorySchema = mongoose.Schema({
-
+    name: {
+        type: String,
+        required: true,
+    },
+    icon: {
+        type: String,
+    },
+    color: { 
+        type: String,
+    }
 })
 
+
+categorySchema.method('toJSON', function(){
+    const { __v, ...object } = this.toObject();
+    const { _id:id, ...result } = object;
+    return { ...result, id };
+});
+
 exports.Category = mongoose.model('Category', categorySchema);
+
+
